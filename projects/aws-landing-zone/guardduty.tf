7544bddc9ef537f892d7cf0d691709cd5cce3422
nanand1806 <n.anand1806@gmail.com>
2026-06-14 20:59:31 +0530

    Simplify GuardDuty to enable-only (drop deprecated datasources block)

--- projects/aws-landing-zone/guardduty.tf
+++ projects/aws-landing-zone/guardduty.tf
@@ -1,12 +1,7 @@
 # GuardDuty is managed threat detection. It continuously analyzes
 # CloudTrail, VPC flow logs, and DNS logs for malicious activity. Enabling
-# it is one resource and is part of any serious account baseline.
+# it is one resource and is part of any serious account baseline. Extra
+# protections (S3, EKS, malware) are added with aws_guardduty_detector_feature.
 resource "aws_guardduty_detector" "this" {
   enable = true
-
-  datasources {
-    s3_logs {
-      enable = true
-    }
-  }
 }
